fda6506e0ede28107ba3b3cef487b72d43d1469c
Nickolas Fotopoulos <nickolas.fotopoulos@synaptics.com>
2013-12-07 16:12:32 -0800

    Add example code from AndroidImageCrop to select image and crop it

---
+++ src/com/example/whiteeye/CropOption.java
@@ -0,0 +1,10 @@
+package com.example.whiteeye;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+public class CropOption {
+	public CharSequence title;
+	public Drawable icon;
+	public Intent appIntent;
+}
